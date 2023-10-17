b6e1becd77ba05d6c58b2317260e85861559ef1f
Orzy895 <enrique.wen@utp.ac.pa>
2023-10-16 21:47:39 -0500

    Finalizacion tarea 4

--- tarea4/index.js
+++ tarea4/index.js
@@ -1,35 +1,39 @@
 let login = document.querySelector('.login')
-let si = document.querySelector('.si')
-let no = document.querySelector('.no')
+let loader = document.querySelector('.loader')
 let password = document.querySelector('#pass')
 let ojo = document.querySelector('.ojo')
+let video = document.querySelector('.video')
+let grua = document.querySelector('.grua')
+let fail = document.getElementById('fail');
+let start = document.getElementById('start');
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function enviar(){
-    await delay(4000);
     if(document.querySelector('#funciona').checked){
-        login.classList.add('esconder')
-        si.classList.remove('esconder')
+        login.classList.add('oculto')
+        loader.classList.remove('oculto')
+        start.play()
+        await delay(3000);
+        loader.classList.add('oculto')
+        video.classList.remove('oculto')
     }
     else{
-        login.classList.add('esconder')
-        no.classList.remove('esconder')
+        login.classList.add('oculto')
+        loader.classList.remove('oculto')
+        fail.play()
+        await delay(4000);
+        loader.classList.add('oculto')
+        grua.classList.remove('oculto')
     }
-}
-
-function regresar(){
-    si.classList.add('esconder')
-    no.classList.add('esconder')
-    login.classList.remove('esconder')
 }
 
 function mostrarPassword(){
     if(password.type == 'password'){
         password.type="text"
-        ojo.src= '/tarea4/imagenes/eye.png'
+        ojo.src= '/tarea4/imagenes/ojo.png'
     }
     else{
         password.type="password"
-        ojo.src='/tarea4/imagenes/hidden.png'
+        ojo.src= '/tarea4/imagenes/cerrar-ojo.png'
     }
 }
